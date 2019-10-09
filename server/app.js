bd49529936033793268443084c84e4da099b18ce
Pooja Rai <raip2812@gmail.com>
2019-10-09 16:36:55 +0530

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+var cors = require('cors')
+
+app.use(cors()) // Use this after the variable declaration
+app.use(bodyParser.json());
+app.use("/",expressJwt({secret: 'todo-app-super-shared-secret'}).unless({path: ['/api/auth']}));
+
+app.post('/api/auth', function(req, res) {
+    
+    const body = req.body;
+    
+    var token = jwt.sign({userID: req.username}, 'todo-app-super-shared-secret', {expiresIn: '2h'});
+    res.send({token});
+});
+
+app.listen(4000, function () {
+    console.log('Angular Canteen Management API Server listening on port 4000!')
+});
